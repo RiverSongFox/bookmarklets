ab885b7e183fdff7deb734dc2624d5169bca025c
Nikolai Kim <nikolai.kim@viamo.io>
2022-06-23 20:46:13 +0700

    chore: set up ssg

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static'
+
+const dev = process.env.NODE_ENV === 'development'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      precompress: true
+    }),
+    prerender: {
+      default: true
+    },
+    paths: {
+      base: dev ? '' : '/bookmarklets'
+    }
+  }
+}
+
+export default config
